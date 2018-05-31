b3c7f9ba13ab9f4f1ba539f36ad7bb4dccd1b4ac
Alix-w7 <39156960+Alix-w7@users.noreply.github.com>
2018-05-31 19:15:10 +0800

    alix 1.0
    alix 1.0

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-//app.js
 App({
 
   globalData: {
+
     userInfo: null,
     server_base: "https://music-link.caoyu.online",
   },
 
+  // 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    
+    var storageData = wx.getStorageSync('worldList');
+    if (!storageData) {
+      //当本地缓存不存在时，再缓存
+      var dataObj = require("data/data.js");
+      wx.clearStorageSync();
+      wx.setStorageSync('worldList', dataObj.worldList);
+    }
   },
-
 
 })
--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,6 +1,6 @@
-// pages/welcome/welcome.js
 
 const app = getApp();
+
 Page({
   data: {
     motto: '开始你的音乐之旅',
@@ -9,19 +9,15 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
-  
+
   onLoad: function () {
-
-
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       });
 
-      
-
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -46,42 +42,30 @@ Page({
 
   },
 
-  enterPostPage:function(){
-    wx.navigateTo({
-      url: '../post/post'
-    })
-  },
-
   getAllAuthorized: function (e) {
-
     //获取用户信息
     this.getUserInfo(e);
-
     //获取录音权限
     this.getRecorderAuthority();
-
-    
-
   },
 
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log("UserInfo getted");
     app.globalData.userInfo = e.detail.userInfo
+    
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     });
-
     console.log(e.detail.rawData);
 
-    this.login(e,(err,res)=>{
+    this.login(e, (err, res) => {
       if (err) return console.log('login function has error');
     })
 
   },
 
-  getRecorderAuthority:function (){
-
+  getRecorderAuthority: function () {
     //获取录音权限
     wx.authorize({
       scope: 'scope.record',
@@ -98,13 +82,13 @@ Page({
           confirmColor: "#52a2d8",
           success: function (res) {
             //确认打开设置界面进行授权
-            if (res.confirm){
-                wx.showModal({
-                  title: '提示',
-                  content: '未授权，录音功能无法使用，刚刚干嘛去了',
-                  showCancel: true,
-                  confirmText: "知道了",
-                })
+            if (res.confirm) {
+              wx.showModal({
+                title: '提示',
+                content: '未授权，录音功能无法使用，刚刚干嘛去了',
+                showCancel: true,
+                confirmText: "知道了",
+              })
             }
           },
           fail: function () {
@@ -113,7 +97,7 @@ Page({
         });
       }
     });
-    
+
   },
 
   login(userinfo, callback) {
@@ -147,8 +131,24 @@ Page({
       },
       fail: function (res) {
       }
-
     }) //只有需要使用微信登录鉴别用户，才需要用到它，用来获取用户的匿名识别符
+  },
+
+  enterPostPage: function (event) {
+    wx.switchTab({
+      url: '../world/world',
+    })
   }
 
 })
+
+
+
+
+
+
+
+
+
+
+
